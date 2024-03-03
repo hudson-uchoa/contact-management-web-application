a24e8aff3aed27d2867de514d1274c403fbf627f
hudson-uchoa <uchoa.hudson1@gmail.com>
2024-03-03 20:40:55 +0000

    feat(auth): Update home route to interact with the user when logged in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,17 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-8 offset-md-2 text-center">
-                <h1>Welcome to Contact Management</h1>
+                @if(auth()->check())
+                    <h1>Welcome back, {{ auth()->user()->name }}!</h1>
+                @else
+                    <h1>Welcome to Contact Management</h1>
+                @endif
                 <p class="lead">A simple and efficient way to manage your contacts.</p>
-                <p>Sign in to start managing your contacts or create a new account if you don't have one yet.</p>
+                @if(auth()->check())
+                    <p>You can now start managing your contacts by clicking the link below.</p>
+                @else
+                    <p>Sign in to start managing your contacts or create a new account if you don't have one yet.</p>
+                @endif
                 <div class="mt-4">
                     @if(auth()->check())
                         <a href="{{ route('contacts.index') }}" class="btn btn-primary">View Contacts</a>
