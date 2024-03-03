60d314ba3f1853cfbe9f9dcdfc8ce4f8ec8f8b1c
hudson-uchoa <uchoa.hudson1@gmail.com>
2024-03-03 18:26:30 +0000

    feat(routes): Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +19,19 @@ Route::group(['middleware' => 'web'], function (){
     })->name('home');
     
     Route::get('/login', 'App\Http\Controllers\AuthController@showLoginForm')->name('login');
-    
     Route::post('/login', 'App\Http\Controllers\AuthController@login');
-    
     Route::get('/register', 'App\Http\Controllers\AuthController@showRegistrationForm')->name('register');
-    
     Route::post('/register', 'App\Http\Controllers\AuthController@register');
+    Route::post('/logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
+    
+    
+    Route::middleware('auth')->prefix('contacts')->name('contacts.')->group(function () {
+        Route::get('/', 'App\Http\Controllers\ContactController@index')->name('index')->withoutMiddleware('auth');
+        Route::get('/create', 'App\Http\Controllers\ContactController@create')->name('create');
+        Route::post('/', 'App\Http\Controllers\ContactController@store')->name('store');
+        Route::get('/{contact}', 'App\Http\Controllers\ContactController@show')->name('show')->where('contact', '[0-9]+');
+        Route::get('/edit/{contact}', 'App\Http\Controllers\ContactController@edit')->name('edit')->where('contact', '[0-9]+');
+        Route::put('/{contact}', 'App\Http\Controllers\ContactController@update')->name('update')->where('contact', '[0-9]+');
+        Route::delete('/{contact}', 'App\Http\Controllers\ContactController@destroy')->name('destroy')->where('contact', '[0-9]+');
+    });
 });
