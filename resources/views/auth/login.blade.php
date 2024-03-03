9a056cb3da26386d3cb4d41baeb5478d189dca38
hudson-uchoa <uchoa.hudson1@gmail.com>
2024-03-03 20:52:09 +0000

    refactor(auth): Remove remember input from login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,10 +34,6 @@
                                     </span>
                                 @enderror
                             </div>
-                            <div class="mb-3 form-check">
-                                <input type="checkbox" class="form-check-input" id="remember" name="remember">
-                                <label class="form-check-label" for="remember">Remember</label>
-                            </div>
                             <button type="submit" class="btn btn-primary">Login</button>
                         </form>
                     </div>
